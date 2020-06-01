37483303292afb4e6a1d8b158341aa5d89aefbc7
r-vvch <romanavov@gmail.com>
2020-06-01 15:42:23 +0300

    maximizer added

---
+++ 2_week/functions/maximizer/update_if_greater.cpp
@@ -0,0 +1,5 @@
+void UpdateIfGreater(int first, int& second) {
+    if (first > second) {
+        second = first;
+    }
+}
---
+++ 2_week/functions/maximizer/update_if_greater.h
@@ -0,0 +1,8 @@
+//
+// Created by roman on 01.06.2020.
+//
+
+#ifndef MAXIMIZER_UPDATE_IF_GREATER_H
+#define MAXIMIZER_UPDATE_IF_GREATER_H
+void UpdateIfGreater(int first, int& second);
+#endif //MAXIMIZER_UPDATE_IF_GREATER_H
