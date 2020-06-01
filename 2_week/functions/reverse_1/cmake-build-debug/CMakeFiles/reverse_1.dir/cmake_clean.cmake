9bddc747e2ebd5e5428c2251cddd9cec856432d8
r-vvch <romanavov@gmail.com>
2020-06-01 20:52:07 +0300

    reverse_1 added

---
+++ 2_week/functions/reverse_1/cmake-build-debug/CMakeFiles/reverse_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/reverse_1.dir/main.cpp.o"
+  "reverse_1"
+  "reverse_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/reverse_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
