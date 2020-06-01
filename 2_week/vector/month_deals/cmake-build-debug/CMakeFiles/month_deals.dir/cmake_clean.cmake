b67f307c79a6e2c1bcfef49d190669695594c1c2
r-vvch <romanavov@gmail.com>
2020-06-01 21:23:30 +0300

    month_deals added

---
+++ 2_week/vector/month_deals/cmake-build-debug/CMakeFiles/month_deals.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/month_deals.dir/main.cpp.o"
+  "month_deals"
+  "month_deals.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/month_deals.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
