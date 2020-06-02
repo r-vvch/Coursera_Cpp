ec67536febfb1ed0d6aa795e60f832202a3da44d
r-vvch <romanavov@gmail.com>
2020-06-02 10:57:47 +0300

    remove old directories

--- 2_week/functions/move_strings/move_strings.cpp
+++
@@ -1,9 +0,0 @@
-#include <vector>
-#include <string>
-
-void MoveStrings(std::vector<std::string>& src, std::vector<std::string>& dst) {
-    for (const auto& i : src) {
-        dst.push_back(i);
-    }
-    src.clear();
-}
--- 2_week/functions/move_strings/move_strings.h
+++
@@ -1,9 +0,0 @@
-#ifndef MOVESTRINGS_MOVESTRINGS_H
-#define MOVESTRINGS_MOVESTRINGS_H
-
-#include <vector>
-#include <string>
-
-void MoveStrings(std::vector<std::string>& src, std::vector<std::string>& dst);
-
-#endif //MOVESTRINGS_MOVESTRINGS_H
