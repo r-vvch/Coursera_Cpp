3a3f1407b3a14115477c95470c109d5de85fb24e
r-vvch <romanavov@gmail.com>
2020-06-02 11:29:23 +0300

    bus_stops_1 added

---
+++ White_belt/2_week/map/bus_stops_1/cmake-build-debug/CMakeFiles/bus_stops_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bus_stops_1.dir/main.cpp.o"
+  "bus_stops_1"
+  "bus_stops_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bus_stops_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
