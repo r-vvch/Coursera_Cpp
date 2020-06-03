bb673076f707ce060d9f9ef744dbf4bf7fb54873
r-vvch <romanavov@gmail.com>
2020-06-03 22:29:11 +0300

    clear.

--- White_belt/2_week/functions/maximizer/update_if_greater.cpp
+++
@@ -1,5 +0,0 @@
-void UpdateIfGreater(int first, int& second) {
-    if (first > second) {
-        second = first;
-    }
-}
--- White_belt/2_week/functions/maximizer/update_if_greater.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by roman on 01.06.2020.
-//
-
-#ifndef MAXIMIZER_UPDATE_IF_GREATER_H
-#define MAXIMIZER_UPDATE_IF_GREATER_H
-void UpdateIfGreater(int first, int& second);
-#endif //MAXIMIZER_UPDATE_IF_GREATER_H
