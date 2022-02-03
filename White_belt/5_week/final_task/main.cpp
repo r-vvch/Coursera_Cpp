08945ce054ce8280ed4d223c93af223c15e63bd5
r-vvch <vovchenko.ra@phystech.edu>
2022-02-03 13:52:49 +0300

    final_task pt.1

---
+++ White_belt/5_week/final_task/main.cpp
@@ -0,0 +1,152 @@
+#include <iostream>
+#include <iomanip>
+#include <map>
+#include <set>
+
+using namespace std;
+
+class Date {
+public:
+    Date(const string& s) {
+        size_t i = 0;
+        try {
+            while (s[i] != '-') {
+                i++;
+            }
+            year = stoi(s.substr(0, i));
+            i++;
+            int pos = i;
+            while (s[i] != '-') {
+                i++;
+            }
+            month = stoi(s.substr(pos, i - pos));
+            day = stoi(s.substr(i + 1, s.length() - i - 1));
+        } catch (exception& e) {
+            cout << "Wrong date format:" + s;
+        }
+    }
+    int GetYear() const {
+        return year;
+    }
+    int GetMonth() const {
+        return month;
+    }
+    int GetDay() const {
+        return day;
+    }
+
+private:
+    int year;
+    int month;
+    int day;
+};
+
+bool operator<(const Date& lhs, const Date& rhs) {
+    if (lhs.GetYear() == rhs.GetYear()) {
+        if (lhs.GetMonth() == rhs.GetMonth()) {
+            return lhs.GetDay() < lhs.GetDay();
+        } else {
+            return lhs.GetMonth() < rhs.GetMonth();
+        }
+    } else {
+        return lhs.GetYear() < rhs.GetYear();
+    }
+}
+
+class Database {
+public:
+    void AddEvent(const Date& date, const string& event) {
+        try {
+            events[date].insert(event);
+        } catch (exception& e) {
+            events[date] = {};
+            events[date].insert(event);
+        }
+    }
+
+    bool DeleteEvent(const Date& date, const string& event) {
+        try {
+            if (events[date].count(event) == 1) {
+                events[date].erase(event);
+                cout << "Deleted successfully\n";
+                return true;
+            } else {
+                cout << "Event not found\n";
+                return false;
+            }
+        } catch (exception& e) {
+            cout << "Event not found\n";
+            return false;
+        }
+    }
+
+    int DeleteDate(const Date& date) {
+        try {
+            size_t n = events[date].size();
+            events.erase(date);
+            cout << "Deleted " + to_string(n) + " events\n";
+            return int(n);
+        } catch (exception& e) {
+            cout << "Deleted 0 events\n";
+            return 0;
+        }
+    }
+
+    set<string> Find(const Date& date) const {
+        for (const auto& i : events.at(date)) {
+            cout << i;
+        }
+        cout << "\n";
+        return events.at(date);
+    }
+
+    void Print() const {
+        for (const auto& i : events) {
+            for (const auto& j : events.at(i.first)) {
+                cout << setw(4) << setfill('0') << i.first.GetYear() << "-"
+                     << setw(2) << setfill('0') << i.first.GetMonth() << "-"
+                     << setw(2) << setfill('0') << i.first.GetDay()
+                     << " " << j;
+            }
+        }
+        cout << '\n';
+    }
+
+private:
+    map<Date, set<string>> events = {};
+};
+
+int main() {
+    Database db;
+
+    string command;
+    while (getline(cin, command)) {
+        // Считайте команды с потока ввода и обработайте каждую
+        string token = command.substr(0, command.find(' '));
+        string command_rest = command.substr(command.find(' ') + 1, command.length() - command.find(' '));
+        if (token == "Add") {
+            string date = command_rest.substr(0, command_rest.find(' '));
+            string event = command_rest.substr(command_rest.find(' ') + 1,
+                                               command_rest.length() - command_rest.find(' ') - 1);
+            db.AddEvent(Date(date), event);
+        } else if (token == "Del") {
+            if (command_rest.find(' ') == string::npos) {
+                db.DeleteDate(Date(command_rest));
+            } else {
+                string date = command_rest.substr(0, command_rest.find(' '));
+                string event = command_rest.substr(command_rest.find(' ') + 1,
+                                                   command_rest.length() - command_rest.find(' ') - 1);
+                Date date_date = Date(date);
+                db.DeleteEvent(date_date, event);
+            }
+        } else if (token == "Find") {
+            db.Find(Date(command_rest));
+        } else if (token == "Print") {
+            db.Print();
+        } else {
+            // ERROR
+        }
+    }
+
+    return 0;
+}
